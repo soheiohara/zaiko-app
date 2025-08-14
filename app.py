9578d4058e176795a91b4cacf582a9c3f596a601
soheiohara <iima7abc@gmail.com>
2025-08-14 21:16:25 +0900

    Fix final indentation error in app.py

--- app.py
+++ app.py
@@ -130,3 +130,8 @@ def update(item_id):
     return redirect(url_for('index'))
 
 @app.route('/forecast',
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
